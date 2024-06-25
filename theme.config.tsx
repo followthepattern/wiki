d2516fc0d5ab49e945f49d8a8efe0ba9ed4dc021
huszkacs <huszkacs@gmail.com>
2024-06-26 00:33:17 +0800

    Adds fp analytics

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,26 +4,27 @@ import Logo from './components/logo/Logo'
 import { useRouter } from 'next/router'
 import { useConfig } from 'nextra-theme-docs'
 import { DiscordLink, WikiLink } from './lib/constants'
- 
+import Script from 'next/script'
 
 function Head() {
-    const { asPath, defaultLocale, locale } = useRouter()
-    const { title, frontMatter } = useConfig()
-    const url =
-      WikiLink +
-      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
- 
-    return (
-      <>
-        <meta property="og:url" content={url} />
-        <meta property="og:title" content={title || 'Follow The Pattern'} />
-        <meta
-          property="og:description"
-          content={frontMatter.description || 'Modern web application development'}
-        />
-      </>
-    )
-  }
+  const { asPath, defaultLocale, locale } = useRouter()
+  const { title, frontMatter } = useConfig()
+  const url =
+    WikiLink +
+    (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+  return (
+    <>
+      <Script src="https://followthepattern.s3.us-east-2.amazonaws.com/fp/fp-analytics.js"/>
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title || 'Follow The Pattern'} />
+      <meta
+        property="og:description"
+        content={frontMatter.description || 'Modern web application development'}
+      />
+    </>
+  )
+}
 
 const config: DocsThemeConfig = {
   logo: Logo,
